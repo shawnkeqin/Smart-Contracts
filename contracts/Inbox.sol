067af49c6003f37ad5e6540095e60aadac68bb05
Shawn <e0052482@u.nus.edu>
2022-04-15 00:06:07 +0800

    require

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -22,9 +22,15 @@ contract Inbox {
 
 contract Lottery {
     address public manager; 
+    address[] public players; 
 
     function Lottery() public {
         manager = msg.sender; 
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);      
     }
 }
 
