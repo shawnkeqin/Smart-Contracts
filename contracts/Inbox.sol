ec78b9df745c57831e193882b5b50878c044a7c8
Shawn <e0052482@u.nus.edu>
2022-04-14 23:52:39 +0800

    arrays

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -27,3 +27,25 @@ contract Lottery {
         manager = msg.sender; 
     }
 }
+
+contract Test {
+    uint[] public myArray; 
+
+    function Test() public {
+        myArray.push(1);
+        myArray.push(2);
+        myArray.push(3);
+    }
+
+    function getMyArray() public view returns (uint[]){
+        return myArray; 
+    }
+
+    function getArrayLength() public view returns (uint){
+        return myArray.length; 
+    }
+
+    function getFirstElement() public view returns (uint){
+        return myArray[0]; 
+    }
+}
