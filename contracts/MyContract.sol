d188d2547dbb0f065bcee80d5294be557a31c6c4
Shawn <e0052482@u.nus.edu>
2022-04-21 23:31:56 +0800

    only owner modifier

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -6,13 +6,23 @@ contract MyContract {
 
     address owner; 
 
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     struct Person {
         uint _id; 
         string _firstName;
         string _lastName;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public {
+     constructor() public {
+         owner = msg.sender;
+    }
+
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner {
         incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName); 
          
